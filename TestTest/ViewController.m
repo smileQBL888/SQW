6cc0dd39adb5b69ca28ac88876f362dfba110983
lixiaodong <xiaodong7@eastmoney.com>
2017-04-19 17:04:58 +0800

    我真的快疯掉了

---
+++ TestTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TestTest
+//
+//  Created by lxd on 2017/4/18.
+//  Copyright © 2017年 QBL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
